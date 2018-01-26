653024c06556b99ce2325ffe41a6666ef9b3688b
Stuart Littlefair <s.littlefair@shef.ac.uk>
2018-01-26 14:29:59 +0000

    Bump version: 0.4.1 → 0.4.2

--- hcam_finder/__init__.py
+++ hcam_finder/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Stuart Littlefair"""
 __email__ = 's.littlefair@shef.ac.uk'
-__version__ = '__version__ = '0.4.1''
+__version__ = '__version__ = '0.4.2''
 
 from .hcam_finder import *
