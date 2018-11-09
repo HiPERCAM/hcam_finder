5ff4ad7082492f15060436cd5d6830125818751b
Stuart Littlefair <s.littlefair@shef.ac.uk>
2018-11-09 09:31:40 +0000

    new values for COMPOs patrol arc

--- hcam_finder/shapes.py
+++ hcam_finder/shapes.py
@@ -55,9 +55,9 @@ class CompoPatrolArc(Polygon):
         # the centre of COMPO's arc describes a circular arc of 48.551' around
         # a point which is 51.1743' below the rotator centre.
         # the radius of the COMPO pickoff mirror is 24"
-        pickoff_size =24*u.arcsec
-        alpha = 5.15965*u.arcmin
-        beta = 5.46316*u.arcmin
+        pickoff_size = 24*u.arcsec
+        alpha = 5.1851*u.arcmin
+        beta = 5.4628*u.arcmin
         r_inner = alpha - pickoff_size/2
         r_outer = alpha + pickoff_size/2
         dec_off = beta.to(u.deg).value
